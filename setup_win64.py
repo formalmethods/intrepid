54aa10328497a986a5a69b7a22638fac58a20095
Roberto Bruttomesso <roberto.bruttomesso@gmail.com>
2016-09-23 22:22:52 +0200

    Fixes to win script

--- setup_win64.py
+++ setup_win64.py
@@ -5,6 +5,11 @@ Setup script for Intrepid Model Checker Python package
 
 from distutils.core import setup
 
+print 'Copying osx libraries'
+shutils.copy('libs/win64/libz3.dll', 'intrepid')
+shutils.copy('libs/win64/libintrepid_dll.dll', 'intrepid')
+shutils.copy('libs/win64/_api.pyd', 'intrepid')
+
 setup(name='intrepid',
       version='0.1',
       description='Intrepid Model Checker',

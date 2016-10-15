c8f3a14279b9196cc076345ad6aba312abd85538
Roberto Bruttomesso <roberto.bruttomesso@gmail.com>
2016-10-15 23:08:05 +0200

    Removed st files, and fixed the api

--- test/test_api.py
+++ test/test_api.py
@@ -10,33 +10,29 @@ class Test_test_api(unittest.TestCase):
         Int.del_ctx(self.ctx)
 
     def test_and(self):
-        circuit = Int.mk_circuit(self.ctx, "tmp")
         bt = Int.mk_boolean_type(self.ctx)
-        x = Int.mk_input(self.ctx, circuit, "x", bt)
-        y = Int.mk_input(self.ctx, circuit, "y", bt)
+        x = Int.mk_input(self.ctx, "x", bt)
+        y = Int.mk_input(self.ctx, "y", bt)
         gate = Int.mk_and(self.ctx, x, y)
         self.assertTrue(Int.is_and(self.ctx, gate))
 
     def test_or(self):
-        circuit = Int.mk_circuit(self.ctx, "tmp")
         bt = Int.mk_boolean_type(self.ctx)
-        x = Int.mk_input(self.ctx, circuit, "x", bt)
-        y = Int.mk_input(self.ctx, circuit, "y", bt)
+        x = Int.mk_input(self.ctx, "x", bt)
+        y = Int.mk_input(self.ctx, "y", bt)
         gate = Int.mk_or(self.ctx, x, y)
         self.assertTrue(Int.is_or(self.ctx, gate))
 
     def test_not(self):
-        circuit = Int.mk_circuit(self.ctx, "tmp")
         bt = Int.mk_boolean_type(self.ctx)
-        x = Int.mk_input(self.ctx, circuit, "x", bt)
+        x = Int.mk_input(self.ctx, "x", bt)
         gate = Int.mk_not(self.ctx, x)
         self.assertTrue(Int.is_not(self.ctx, gate))
 
     def test_xor(self):
-        circuit = Int.mk_circuit(self.ctx, "tmp")
         bt = Int.mk_boolean_type(self.ctx)
-        x = Int.mk_input(self.ctx, circuit, "x", bt)
-        y = Int.mk_input(self.ctx, circuit, "y", bt)
+        x = Int.mk_input(self.ctx, "x", bt)
+        y = Int.mk_input(self.ctx, "y", bt)
         gate = Int.mk_xor(self.ctx, x, y)
         # This is intended behavior, because of internal
         # simplifications (xor a b) is rewritten into
@@ -44,93 +40,82 @@ class Test_test_api(unittest.TestCase):
         self.assertTrue(Int.is_iff(self.ctx, gate))
 
     def test_iff(self):
-        circuit = Int.mk_circuit(self.ctx, "tmp")
         bt = Int.mk_boolean_type(self.ctx)
-        x = Int.mk_input(self.ctx, circuit, "x", bt)
-        y = Int.mk_input(self.ctx, circuit, "y", bt)
+        x = Int.mk_input(self.ctx, "x", bt)
+        y = Int.mk_input(self.ctx, "y", bt)
         gate = Int.mk_iff(self.ctx, x, y)
         self.assertTrue(Int.is_iff(self.ctx, gate))
 
     def test_eq(self):
-        circuit = Int.mk_circuit(self.ctx, "tmp")
         i8t = Int.mk_int8_type(self.ctx)
-        x = Int.mk_input(self.ctx, circuit, "x", i8t)
-        y = Int.mk_input(self.ctx, circuit, "y", i8t)
+        x = Int.mk_input(self.ctx, "x", i8t)
+        y = Int.mk_input(self.ctx, "y", i8t)
         gate = Int.mk_eq(self.ctx, x, y)
         self.assertTrue(Int.is_eq(self.ctx, gate))
 
     def test_leq(self):
-        circuit = Int.mk_circuit(self.ctx, "tmp")
         i8t = Int.mk_int8_type(self.ctx)
-        x = Int.mk_input(self.ctx, circuit, "x", i8t)
-        y = Int.mk_input(self.ctx, circuit, "y", i8t)
+        x = Int.mk_input(self.ctx, "x", i8t)
+        y = Int.mk_input(self.ctx, "y", i8t)
         gate = Int.mk_leq(self.ctx, x, y)
         self.assertTrue(Int.is_leq(self.ctx, gate))
 
     def test_lt(self):
-        circuit = Int.mk_circuit(self.ctx, "tmp")
         i8t = Int.mk_int8_type(self.ctx)
-        x = Int.mk_input(self.ctx, circuit, "x", i8t)
-        y = Int.mk_input(self.ctx, circuit, "y", i8t)
+        x = Int.mk_input(self.ctx, "x", i8t)
+        y = Int.mk_input(self.ctx, "y", i8t)
         gate = Int.mk_lt(self.ctx, x, y)
         self.assertTrue(Int.is_not(self.ctx, gate))
 
     def test_geq(self):
-        circuit = Int.mk_circuit(self.ctx, "tmp")
         i8t = Int.mk_int8_type(self.ctx)
-        x = Int.mk_input(self.ctx, circuit, "x", i8t)
-        y = Int.mk_input(self.ctx, circuit, "y", i8t)
+        x = Int.mk_input(self.ctx, "x", i8t)
+        y = Int.mk_input(self.ctx, "y", i8t)
         gate = Int.mk_geq(self.ctx, x, y)
         self.assertTrue(Int.is_leq(self.ctx, gate))
 
     def test_gt(self):
-        circuit = Int.mk_circuit(self.ctx, "tmp")
         i8t = Int.mk_int8_type(self.ctx)
-        x = Int.mk_input(self.ctx, circuit, "x", i8t)
-        y = Int.mk_input(self.ctx, circuit, "y", i8t)
+        x = Int.mk_input(self.ctx, "x", i8t)
+        y = Int.mk_input(self.ctx, "y", i8t)
         gate = Int.mk_gt(self.ctx, x, y)
         self.assertTrue(Int.is_not(self.ctx, gate))
 
     def test_input(self):
-        circuit = Int.mk_circuit(self.ctx, "tmp")
         bt = Int.mk_boolean_type(self.ctx)
-        x = Int.mk_input(self.ctx, circuit, "x", bt)
-        self.assertTrue(Int.is_input(self.ctx, circuit, x))
+        x = Int.mk_input(self.ctx, "x", bt)
+        self.assertTrue(Int.is_input(self.ctx, x))
 
     def test_inputs_number(self):
-        circuit = Int.mk_circuit(self.ctx, "tmp")
         bt = Int.mk_boolean_type(self.ctx)
-        x = Int.mk_input(self.ctx, circuit, "x", bt)
-        y = Int.mk_input(self.ctx, circuit, "y", bt)
-        self.assertEquals(2, Int.get_inputs_size(circuit))
+        x = Int.mk_input(self.ctx, "x", bt)
+        y = Int.mk_input(self.ctx, "y", bt)
+        self.assertEquals(2, Int.get_inputs_number(ctx))
 
     def test_output(self):
-        circuit = Int.mk_circuit(self.ctx, "tmp")
         bt = Int.mk_boolean_type(self.ctx)
-        x = Int.mk_input(self.ctx, circuit, "x", bt)
-        y = Int.mk_input(self.ctx, circuit, "y", bt)
+        x = Int.mk_input(self.ctx, "x", bt)
+        y = Int.mk_input(self.ctx, "y", bt)
         z = Int.mk_and(self.ctx, x, y)
-        Int.mk_output(self.ctx, circuit, "z", z)
-        self.assertTrue(Int.is_output(self.ctx, circuit, z))
-        self.assertEquals(1, Int.get_outputs_size(circuit))
+        Int.mk_output(self.ctx, "z", z)
+        self.assertTrue(Int.is_output(self.ctx, z))
+        self.assertEquals(1, Int.get_outputs_number(ctx))
 
     def test_latch(self):
-        circuit = Int.mk_circuit(self.ctx, "tmp")
         bt = Int.mk_boolean_type(self.ctx)
-        x = Int.mk_input(self.ctx, circuit, "x", bt)
-        l = Int.mk_latch(self.ctx, circuit, "l", bt)
+        x = Int.mk_input(self.ctx, "x", bt)
+        l = Int.mk_latch(self.ctx, "l", bt)
         t = Int.mk_true(self.ctx)
-        Int.set_latch_init_next(self.ctx, circuit, l, t, x)
-        self.assertTrue(Int.is_latch(self.ctx, circuit, l))
+        Int.set_latch_init_next(self.ctx, l, t, x)
+        self.assertTrue(Int.is_latch(self.ctx, l))
 
     def test_bmc(self):
-        circuit = Int.mk_circuit(self.ctx, "tmp")
         bt = Int.mk_boolean_type(self.ctx)
-        x = Int.mk_input(self.ctx, circuit, "x", bt)
-        y = Int.mk_input(self.ctx, circuit, "y", bt)
+        x = Int.mk_input(self.ctx, "x", bt)
+        y = Int.mk_input(self.ctx, "y", bt)
         gate = Int.mk_and(self.ctx, x, y)
-        Int.mk_output(self.ctx, circuit, "out1", gate)
-        bmc = Int.mk_engine_bmc(self.ctx, circuit, 10)
+        Int.mk_output(self.ctx, "out1", gate)
+        bmc = Int.mk_engine_bmc(self.ctx, 10)
         Int.bmc_add_target(self.ctx, bmc, gate)
         Int.set_bmc_current_depth(bmc, 0)
         result = Int.bmc_reach_targets(bmc)

ef7ecf23af89b8dfed5927d12510e739c38ff8f1
Roberto Bruttomesso <roberto.bruttomesso@gmail.com>
2016-11-26 07:58:17 -0800

    Automatic update for linux 32

--- setup.py
+++ setup.py
@@ -11,13 +11,23 @@ import sys
 import os
 
 systemStr = platform.system()
+bits, _ = platform.architecture()
 
 packageDataList = []
 
 if systemStr == 'Linux':
-      print 'Copying linux64 libraries'
-      shutil.copy('libs/linux64/_api.so', 'intrepid')
-      packageDataList = ['_api.so']
+      if bits == 32:
+      	   print 'Copying linux32 libraries'
+      	   shutil.copy('libs/linux32/_api.so', 'intrepid')
+           packageDataList = ['_api.so']
+      elif bits == 64:
+      	   print 'Copying linux64 libraries'
+      	   shutil.copy('libs/linux64/_api.so', 'intrepid')
+           packageDataList = ['_api.so']
+      else:
+           print 'Unsupported number of bits', bits
+           sys.exit(1)
+
 elif systemStr == 'Windows':
       print 'Copying win64 libraries'
       shutil.copy('libs/win64/libz3.dll', 'intrepid')

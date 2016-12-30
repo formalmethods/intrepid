baaf13a15bcff1386d38eb36134ac8c324694da4
Roberto Bruttomesso <roberto.bruttomesso@gmail.com>
2016-12-30 11:10:22 +0100

    Added shell version of counter example

---
+++ examples/counter/counter_exe.py
@@ -0,0 +1,16 @@
+import intrepid as ip
+import intrepid.components
+import intrepid.utils
+
+ctx = ip.mk_ctx()
+int8type = ip.mk_int8_type(ctx)
+ten = ip.mk_number(ctx, "10", int8type)
+counter, Q = ip.components.mk_counter(ctx, "counter", type=int8type, limit=ten)
+simulator = ip.mk_simulator(ctx)
+ip.simulator_add_watch(ctx, simulator, counter)
+ip.simulator_add_watch(ctx, simulator, Q)
+cex = ip.simulator_default_simulate(simulator, 12)
+cexDict = ip.utils.counterexample_get_as_dictionary(ctx, cex, {}, { 'counter' : counter, 'Q' : Q })
+df = ip.utils.counterexample_get_as_dataframe(cexDict)
+print df
+ip.del_ctx(ctx)

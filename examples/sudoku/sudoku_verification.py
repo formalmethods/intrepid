2a1c1cbc84ea5f51c7782dc516dd8d016ac52550
Roberto Bruttomesso <roberto.bruttomesso@gmail.com>
2017-01-19 22:15:18 +0100

    Improvements to sudoku printer

--- examples/sudoku/sudoku_verification.py
+++ examples/sudoku/sudoku_verification.py
@@ -9,11 +9,26 @@ if __name__ == "__main__":
     inst.mk_circuit()
     prsTime = time.time() - startTime
     negProp = inst.proof_objectives['sudoku/Proof Objective']
-    br = ip.mk_engine_br(ctx)
-    ip.br_add_target(ctx, br, negProp)
-    ip.br_add_watch(ctx, br, negProp)
-    result = ip.br_reach_targets(br)
-    print "Result:", result == ip.INT_ENGINE_RESULT_REACHABLE
+    bmc = ip.mk_engine_bmc(ctx)
+    ip.bmc_add_target(ctx, bmc, negProp)
+    ip.bmc_add_watch(ctx, bmc, negProp)
+    result = ip.bmc_reach_targets(bmc)
+    if result == ip.INT_ENGINE_RESULT_REACHABLE:
+        print 'There is a solution:'
+        cex = ip.bmc_get_counterexample(ctx, bmc, negProp)
+        cexDict = ip.utils.counterexample_get_as_dictionary(ctx, cex, inst.inputs, {})
+        cell = 1
+        table = ''
+        for _, value in cexDict.iteritems():
+            table += value[0] + ' '
+            if cell % 9 == 0 and cell != 81:
+                table += '\n'
+            cell += 1
+        print table
+    elif result == ip.INT_ENGINE_RESULT_UNREACHABLE:
+        print 'There is no possible solution'
+    else:
+        print 'An error occurred'
     endTime = time.time() - startTime
     print 'Parse time:', prsTime
     print 'Total time:', endTime

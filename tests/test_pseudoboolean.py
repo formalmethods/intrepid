eec412b745c90ca7529f0cc8ccdbc11c8c20627d
Roberto Bruttomesso <roberto.bruttomesso@gmail.com>
2017-02-03 13:54:43 -0800

    Some fixes

--- tests/test_pseudoboolean.py
+++ tests/test_pseudoboolean.py
@@ -3,18 +3,15 @@ from intrepyd.engine import EngineResult
 import intrepyd.pseudoboolean
 import unittest
 
-class TestPseudoBoolean(unittest.TestCase):
+class TestExactlyOne(unittest.TestCase):
 
-    def setUp(self):
-        self.ctx = ip.Context()
-
-    def create_and_run_nets(self, n):
+    def create_and_run_nets(self, n, context):
         nets = []
-        bt = self.ctx.mk_boolean_type()
+        bt = context.mk_boolean_type()
         for i in range(n):
-            nets.append(self.ctx.mk_input("n" + str(i), bt))
-        eo = ip.pseudoboolean.mk_exactly_one(self.ctx.ctx, nets, "eo")
-        bmc = self.ctx.mk_bmc()
+            nets.append(context.mk_input("n" + str(i), bt))
+        eo = ip.pseudoboolean.mk_exactly_one(context.ctx, nets, "eo")
+        bmc = context.mk_bmc()
         bmc.add_target(eo)
         result = bmc.reach_targets()
         self.assertEqual(EngineResult.REACHABLE, result)
@@ -22,40 +19,52 @@ class TestPseudoBoolean(unittest.TestCase):
         self.assertEqual(1, len(list(reachedNets)))
 
     def test_exactly_one_01(self):
-        self.create_and_run_nets(1)
+        context = ip.Context()
+        self.create_and_run_nets(1, context)
 
     def test_exactly_one_02(self):
-        self.create_and_run_nets(2)
+        context = ip.Context()
+        self.create_and_run_nets(2, context)
 
     def test_exactly_one_03(self):
-        self.create_and_run_nets(3)
+        context = ip.Context()
+        self.create_and_run_nets(3, context)
 
     def test_exactly_one_04(self):
-        self.create_and_run_nets(4)
+        context = ip.Context()
+        self.create_and_run_nets(4, context)
 
     def test_exactly_one_05(self):
-        self.create_and_run_nets(10)
+        context = ip.Context()
+        self.create_and_run_nets(10, context)
 
     def test_exactly_one_06(self):
-        self.create_and_run_nets(11)
+        context = ip.Context()
+        self.create_and_run_nets(11, context)
 
     def test_exactly_one_07(self):
-        self.create_and_run_nets(12)
+        context = ip.Context()
+        self.create_and_run_nets(12, context)
 
     def test_exactly_one_08(self):
-        self.create_and_run_nets(43)
+        context = ip.Context()
+        self.create_and_run_nets(43, context)
 
     def test_exactly_one_09(self):
+        context = ip.Context()
         nets = []
-        bt = self.ctx.mk_boolean_type()
-        i1 = self.ctx.mk_input("i1", bt)
-        i2 = self.ctx.mk_input("i2", bt)
-        i3 = self.ctx.mk_input("i3", bt)
-        nets.append(self.ctx.mk_or(i1, i2))
-        nets.append(self.ctx.mk_or(i1, i3))
-        nets.append(self.ctx.mk_or(i2, i3))
-        eo = ip.pseudoboolean.mk_exactly_one(self.ctx.ctx, nets, "eo")
-        bmc = self.ctx.mk_bmc()
+        bt = context.mk_boolean_type()
+        i1 = context.mk_input("i1", bt)
+        i2 = context.mk_input("i2", bt)
+        i3 = context.mk_input("i3", bt)
+        nets.append(context.mk_or(i1, i2))
+        nets.append(context.mk_or(i1, i3))
+        nets.append(context.mk_or(i2, i3))
+        eo = ip.pseudoboolean.mk_exactly_one(context.ctx, nets, "eo")
+        bmc = context.mk_bmc()
         bmc.add_target(eo)
         result = bmc.reach_targets()
         self.assertEqual(EngineResult.UNREACHABLE, result)
+
+if __name__ == "__main__":
+    unittest.main()

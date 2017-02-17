570e3187ec579a776c5db2be0d51922cfacf03a2
Roberto Bruttomesso <roberto.bruttomesso@gmail.com>
2017-02-17 22:25:43 +0100

    Added test

---
+++ tests/test_engine.py
@@ -0,0 +1,20 @@
+import unittest
+import intrepyd
+import intrepyd.engine
+
+class TestEngine(unittest.TestCase):
+
+    def test_engine_01(self):
+        ctx = intrepyd.Context()
+        bmc = ctx.mk_bmc()
+        br = ctx.mk_backward_reach()
+        bmcOpt = ctx.mk_optimizing_bmc()
+        self.assertEqual(False, bmc.can_prove())
+        self.assertEqual(False, bmc.can_optimize())
+        self.assertEqual(True, br.can_prove())
+        self.assertEqual(False, br.can_optimize())
+        self.assertEqual(False, bmcOpt.can_prove())
+        self.assertEqual(True, bmcOpt.can_optimize())
+
+if __name__ == '__main__':
+    unittest.main()

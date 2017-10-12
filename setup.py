7ca708a4a6cf0dd11ae0e129f6de908221e60c9a
Roberto Bruttomesso <roberto.bruttomesso@axelsw.it>
2017-10-12 14:33:13 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import platform
 import sys
 from setuptools import setup, find_packages
 
-VERSION = '0.5.12'
+VERSION = '0.5.13'
 
 system_str = platform.system()
 bits, _ = platform.architecture()

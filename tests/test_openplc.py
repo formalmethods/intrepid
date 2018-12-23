8a11d6fe3c21da3485e23bb6093a7d1f601e7e39
Roberto Bruttomesso <roberto.bruttomesso@gmail.com>
2018-12-23 16:04:25 +0100

    Debugging

--- tests/test_openplc.py
+++ tests/test_openplc.py
@@ -33,6 +33,10 @@ class TestOpenPLC(unittest.TestCase):
         pous = parsePlcOpenFile('tests/openplc/if3.xml')
         self.assertEquals(1, len(pous))
 
+    def test_if_4(self):
+        pous = parsePlcOpenFile('tests/openplc/if4.xml')
+        self.assertEquals(1, len(pous))
+
     # def test_infusion_pump(self):
     #     pous = parsePlcOpenFile('tests/openplc/GPCA_SW_Functional_subst.xml')
     #     self.assertEquals(1, len(pous))
